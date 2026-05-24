b210faaaca0eddf73e89ccdbfee7054703c2a339
Medijobs <medijobsrd@gmail.com>
2026-05-24 14:34:28 -0400

    refactor: migrar de bcrypt a bcryptjs para compatibilidad de build en Railway

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const cors = require("cors");
 const path = require("path");
 const sqlite3 = require("sqlite3");
